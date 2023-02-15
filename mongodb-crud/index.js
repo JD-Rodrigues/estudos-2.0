abcf28f6fa2c88eb0402f0ddc90a786d3491d49f
JD_Rodrigues <jdevproject01@gmail.com>
2023-02-15 01:10:11 -0300

    Crud MongoRUD MongoDB - função read com parâmetro opcional, para retornar um documento especíofico ou todos

--- mongodb-crud/index.js
+++ mongodb-crud/index.js
@@ -1,18 +1,33 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient} = require('mongodb');
 
 const uri = "mongodb+srv://domingosrodrigues:readyplayer1@cluster0.c9afncg.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri);
 
-const query = async (query) => {
+const read = async (query = {}) => {
   try {
-    const collection = client.db('store').collection('costumers')
-    const doc = await collection.findOne(query)
-    console.log(doc)
+    const db = client.db('store')
+    const collection = db.collection('costumers')
+    const doc = collection.find(query)
+    await doc.forEach(console.dir)
   } finally {
     client.close()
   }
 }
 
-// query({name:'Domingos Rodrigues'}).catch(console.log)
+const create = async (query) => {
+  try {
+    const collection = client.db('store').collection('costumers')
+    await collection.insertOne(query)
+
+  } finally {
+    client.close()
+  }
+}
+
+
+// run().catch(console.dir);
+
+read()
+// create({name:'Sarah Tatyane', email:'contato@taty.com', cod:'dahdjkhjfk0'})
