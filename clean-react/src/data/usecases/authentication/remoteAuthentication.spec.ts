6e12163cf7d8d59e1d0be3f1b9dfe73fd155166c
JD_Rodrigues <jdevproject01@gmail.com>
2023-04-21 21:51:11 -0300

    test: RemoteAuthentication.auth() should call HttpPostClientSpy.post() with the correct url

--- clean-react/src/data/usecases/authentication/remoteAuthentication.spec.ts
+++ clean-react/src/data/usecases/authentication/remoteAuthentication.spec.ts
@@ -1,15 +1,10 @@
 import { HttpPostClient } from "data/protocols/http/httpPostClient";
 import { RemoteAuthentication } from "./remoteAuthentication";
+import { HttpPostClientSpy } from "../../tests/mockRemotePostClient";
 
 
 
 describe('RemoteAuthentication', () => {
-  class HttpPostClientSpy implements HttpPostClient {
-    url?: string
-    async post(path: string): Promise<void> {
-      this.url = path
-    }
-  }
   it('should call httpPostClient with the correct url', ()=> {
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy
