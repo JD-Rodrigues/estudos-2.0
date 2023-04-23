d561a19e3dfdfc0474bb48eb29ea64d854d3897c
JD_Rodrigues <jdevproject01@gmail.com>
2023-04-22 21:19:58 -0300

    refactor: lib faker used in url mock const of the RemoteAuthentication test

--- clean-react/dist/data/usecases/authentication/remoteAuthentication.spec.js
+++ clean-react/dist/data/usecases/authentication/remoteAuthentication.spec.js
@@ -1,8 +1,9 @@
 import { RemoteAuthentication } from "./remoteAuthentication";
 import { HttpPostClientSpy } from "../../tests/mockRemotePostClient";
+import { faker } from '@faker-js/faker';
 describe('RemoteAuthentication', () => {
     it('should call httpPostClient with the correct url', () => {
-        const url = 'any_url';
+        const url = faker.internet.url();
         const httpPostClientSpy = new HttpPostClientSpy;
         const sut = new RemoteAuthentication(url, httpPostClientSpy);
         sut.auth();
--- clean-react/src/data/usecases/authentication/remoteAuthentication.spec.ts
+++ clean-react/src/data/usecases/authentication/remoteAuthentication.spec.ts
@@ -1,12 +1,14 @@
 import { HttpPostClient } from "data/protocols/http/httpPostClient";
 import { RemoteAuthentication } from "./remoteAuthentication";
 import { HttpPostClientSpy } from "../../tests/mockRemotePostClient";
+import { faker } from '@faker-js/faker';
+
 
 
 
 describe('RemoteAuthentication', () => {
   it('should call httpPostClient with the correct url', ()=> {
-    const url = 'any_url'
+    const url = faker.internet.url()
     const httpPostClientSpy = new HttpPostClientSpy
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
     sut.auth()
