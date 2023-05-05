fd5d0183450be2e87703c2c0edd7ac60ce0773dc
JD_Rodrigues <jdevproject01@gmail.com>
2023-05-05 15:02:57 -0300

    doc: poc react-swiper - comentários no código.

--- poc-react-swiper/src/App.jsx
+++ poc-react-swiper/src/App.jsx
@@ -1,22 +1,11 @@
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar } from 'swiper';
-
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import './App.css'
-// eslint-disable-next-line no-unused-vars
-
-
 
 function App() {
-  
-  
-
-  
-
   return (
     <div className='container'>
       <Swiper
@@ -25,18 +14,12 @@ function App() {
       navigation
       pagination 
       loop
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
       >
         <SwiperSlide ><img  src='https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg'/></SwiperSlide>
         <SwiperSlide><img  src='https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp'/></SwiperSlide>
         <SwiperSlide><img  src='https://1.bp.blogspot.com/-5XOjJvW09zQ/UjtEqc523_I/AAAAAAAAmTo/dOHXSTw3pF8/s1600/PAISAGEM.jpg'/></SwiperSlide>
         <SwiperSlide><img  src='https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/14179421596_b95c10db10_o.jpeg?quality=70&strip=info&w=928&w=636'/></SwiperSlide>      
       </Swiper>
-      <button>
-        Botão
-      </button>
-
     </div> 
   );
 }
