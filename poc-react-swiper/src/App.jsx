4d04f9a068cef82506f4b632027a0a9180b5c330
JD_Rodrigues <jdevproject01@gmail.com>
2023-05-05 13:46:58 -0300

    refact: aspect ratio do slide da poc react-swipper alterado para 16/9.

--- poc-react-swiper/src/App.jsx
+++ poc-react-swiper/src/App.jsx
@@ -28,13 +28,11 @@ function App() {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide ><img className='imagem' src='https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg'/></SwiperSlide>
-        <SwiperSlide><img className='imagem' src='https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp'/></SwiperSlide>
-        <SwiperSlide><img className='imagem' src='https://1.bp.blogspot.com/-5XOjJvW09zQ/UjtEqc523_I/AAAAAAAAmTo/dOHXSTw3pF8/s1600/PAISAGEM.jpg'/></SwiperSlide>
-        <SwiperSlide><img className='imagem' src='https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/14179421596_b95c10db10_o.jpeg?quality=70&strip=info&w=928&w=636'/></SwiperSlide>
-      
+        <SwiperSlide ><img  src='https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg'/></SwiperSlide>
+        <SwiperSlide><img  src='https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp'/></SwiperSlide>
+        <SwiperSlide><img  src='https://1.bp.blogspot.com/-5XOjJvW09zQ/UjtEqc523_I/AAAAAAAAmTo/dOHXSTw3pF8/s1600/PAISAGEM.jpg'/></SwiperSlide>
+        <SwiperSlide><img  src='https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/14179421596_b95c10db10_o.jpeg?quality=70&strip=info&w=928&w=636'/></SwiperSlide>      
       </Swiper>
-
       <button>
         Botão
       </button>
