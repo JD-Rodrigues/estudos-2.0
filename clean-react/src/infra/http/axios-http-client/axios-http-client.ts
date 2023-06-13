79873090f7735f4f9e1dfed16f28ac0def258b08
JD_Rodrigues <jdevproject01@gmail.com>
2023-06-13 01:13:43 -0300

    feat: implements class AxiosHttpClient

---
+++ clean-react/src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient, HttpPostParams } from "@/data/protocols/http";
+import { AuthenticationParams } from "@/domain/usercases/authentication";
+import axios from "axios";
+
+
+export class AxiosHttpClient {
+  async post(params:HttpPostParams<AuthenticationParams>): Promise<void> {
+    await axios.post(params.url) 
+  }
+}
+
+ 
