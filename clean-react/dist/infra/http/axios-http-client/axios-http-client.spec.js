c000741e213a9113a4881a406fcd2c6ca61ef52d
JD_Rodrigues <jdevproject01@gmail.com>
2023-06-13 01:16:24 -0300

    refactor: moves AxiosHttpClient to a factory funciton.

--- clean-react/dist/infra/http/axios-http-client/axios-http-client.spec.js
+++ clean-react/dist/infra/http/axios-http-client/axios-http-client.spec.js
@@ -2,9 +2,10 @@ import { faker } from "@faker-js/faker";
 import axios from 'axios';
 import { AxiosHttpClient } from "./axios-http-client";
 jest.mock('axios');
+const makeSut = () => new AxiosHttpClient();
 describe('AxiosHttpClient', () => {
     it('should call axios with the correct url', () => {
-        const sut = new AxiosHttpClient();
+        const sut = makeSut();
         const url = faker.internet.url();
         sut.post({ url: url });
         expect(axios.post).toHaveBeenCalledWith(url);
--- clean-react/src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ clean-react/src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -3,10 +3,11 @@ import axios from 'axios'
 import { AxiosHttpClient } from "./axios-http-client"
 
 jest.mock('axios')
+const makeSut = ()=> new AxiosHttpClient()
 
 describe('AxiosHttpClient', ()=> {
   it('should call axios with the correct url', ()=> {
-    const sut = new AxiosHttpClient()
+    const sut = makeSut()
     const url = faker.internet.url()
     sut.post({url:url})
     expect(axios.post).toHaveBeenCalledWith(url) 
