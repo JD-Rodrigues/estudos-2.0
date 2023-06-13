a0650bd663e45c51e072ef6cea839d7ac6cb33c2
JD_Rodrigues <jdevproject01@gmail.com>
2023-06-13 01:12:45 -0300

    test: ensures AxiosHttpClient calls axios.post with the correct url.

---
+++ clean-react/dist/infra/http/axios-http-client/axios-http-client.spec.js
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+import axios from 'axios';
+import { AxiosHttpClient } from "./axios-http-client";
+jest.mock('axios');
+describe('AxiosHttpClient', () => {
+    it('should call axios with the correct url', () => {
+        const sut = new AxiosHttpClient();
+        const url = faker.internet.url();
+        sut.post({ url: url });
+        expect(axios.post).toHaveBeenCalledWith(url);
+    });
+});
