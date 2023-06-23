d3f53eceff7ce9e9eedacdefb64fb4df454db13c
JD_Rodrigues <jdevproject01@gmail.com>
2023-06-22 23:58:59 -0300

    feat: add webpack.config.js

---
+++ clean-react/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path:path.join(__dirname, 'public/js'), 
+    publicPath: '/public/js',
+    filename:'bundle.js',
+  }, 
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }, 
+  devServer: {
+    contentBase: './public',
+    writeToDisk:true, 
+    historyApiFallback:true
+  }
+}
