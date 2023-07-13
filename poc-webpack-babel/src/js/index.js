950aee0e528b760b1943ebce482c9d079a63252a
JD_Rodrigues <jdevproject01@gmail.com>
2023-07-12 23:28:35 -0300

    chore: add .gitignore

---
+++ poc-webpack-babel/src/js/index.js
@@ -0,0 +1,3 @@
+const name = 'Maria'
+
+module.exports = name
--- poc-webpack-babel/webpack.config.js
+++ poc-webpack-babel/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: '/src/js/index.js',
   output: {
     filename: 'main.js',
-    pathname: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist')
   }
   
 }
