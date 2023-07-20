9d4e1aa4059ee5689f24d95dc29e7c8375bb07b6
JD_Rodrigues <jdevproject01@gmail.com>
2023-07-19 21:37:34 -0300

    add Terser Plugin and configures it in Webpack - poc-webpack-babel

--- poc-webpack-babel/webpack.config.js
+++ poc-webpack-babel/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   mode: "development",
@@ -18,7 +18,8 @@ module.exports = {
         template: '/src/index.html'
       }
     ),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new TerserPlugin()
   ],
   module: {
     rules: [
