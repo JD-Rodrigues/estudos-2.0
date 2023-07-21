dee76b436bf953a38583e73ce09f427392e5d884
JD_Rodrigues <jdevproject01@gmail.com>
2023-07-20 23:39:23 -0300

    refactor: changes the babel rule structure in webpack.config.js, removing the 'use' option- poc-webpack-babel

--- poc-webpack-babel/webpack.config.js
+++ poc-webpack-babel/webpack.config.js
@@ -37,11 +37,9 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
         }
      }
     ]
