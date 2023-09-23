1123a54b7a348027fc4c90a6f7c0ac8ca52337cb
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-23 11:31:29 -0300

    Looking for patterns inside strings

---
+++ MySQL/WHERE IN.sql
@@ -0,0 +1,39 @@
+SELECT
+	*
+FROM
+	users
+WHERE 
+	email
+LIKE
+	'%hotmail%'
+;
+
+SELECT
+	*
+FROM
+	users
+WHERE 
+	created_at
+LIKE
+	'%15%'
+;
+
+SELECT
+	*
+FROM
+	users
+WHERE 
+	email
+LIKE
+	'contato%'
+;
+
+SELECT
+	*
+FROM
+	users
+WHERE 
+	name
+LIKE
+	'%a'
+;
