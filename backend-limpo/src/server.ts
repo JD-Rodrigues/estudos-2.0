b6078b28bc0a3de7cebb0dc71e0e3a489913af22
JD_Rodrigues <jdevproject01@gmail.com>
2023-10-11 06:54:31 -0300

    refactor: changes server from Node HTTP to Express.js

--- backend-limpo/src/server.ts
+++ backend-limpo/src/server.ts
@@ -1,12 +1,31 @@
-import http from 'http'
-import routes from './adapters/example';
+import express, { Request, Response } from 'express';
+import { UserRepositoryController } from './adapters/UserRepositoryController';
+import RegisterUser from './core/usuarios/service/RegisterUser';
+import UserRepositoryMemory from './external/memory/UserRepositoryMemory';
 
-const server = http.createServer(routes);
 
-const porta = 3000;
+var app = express()
+app.use(express.json());
+app.listen(3001)
 
-server.listen(porta, () => {
-  console.log(`Servidor está ouvindo na porta ${porta}`);
-});
 
-export default server
+
+app.post('/costumers',(req:Request, res: Response)=>{
+  const body = req.body
+  const userRepositoryCostumers = new UserRepositoryMemory()
+  const registerUser = new RegisterUser(userRepositoryCostumers)
+  const userRepositoryController = new UserRepositoryController(registerUser, req.body)
+
+  try {
+    userRepositoryController.store()
+    res.send(req.body)
+  } catch (error) {
+    res.send(error)
+  }
+  
+  
+} )
+
+
+
+export default app
