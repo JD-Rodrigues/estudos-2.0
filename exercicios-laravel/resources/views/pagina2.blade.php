c5261c96ac33be468aec041851ae650c2d9ee576
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-02-10 00:01:22 -0300

    feat: implements view pagina2

---
+++ exercicios-laravel/resources/views/pagina2.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout2')
+
+@section('header')
+ <h1>Olá! Eu sou o header!</h1>
+ @parent
+@endsection
+
+
+@section('content')
+    <strong> 'Hoje é um novo dia.'</strong>
+@endsection
