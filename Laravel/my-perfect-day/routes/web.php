1348b035486991996a216f93d844d669209499de
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-03-02 21:30:59 -0300

    feat: implements GET /post route

--- Laravel/my-perfect-day/routes/web.php
+++ Laravel/my-perfect-day/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'authenticate']);
+Route::get('/post', [PostController::class, 'view']);
