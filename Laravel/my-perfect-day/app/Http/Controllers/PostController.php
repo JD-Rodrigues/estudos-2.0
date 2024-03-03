eff5d4b900121cad6a2089160fb884c79e5c9ad2
Domingos Rodrigues <domingosrodrigues.dev@gmail.com>
2024-03-02 23:50:57 -0300

    feat: passes an instance of Post to view

--- Laravel/my-perfect-day/app/Http/Controllers/PostController.php
+++ Laravel/my-perfect-day/app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Policies\PostPolicy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,15 +14,24 @@ class PostController extends Controller
     public function view(Request $request) {
 
         auth()->loginUsingId(1);
-        // dd(auth()->user());
+
         
-        // if(Gate::allows('view', $post)){
+        // auth()->logout();
+        
+        
+        
+
+        // if($this->authorize('view', Post::class)){
         //     return view('post');
         // }
-        if($this->authorize('view', Post::class)){
-            return view('post');
-        }
         
+        // if($request->user()->can('view', Post::class)){
+        //     return view('post');
+        // }else{
+        //     abort(403);
+        // }
+
+        return view('post',['post' => Post::class]);
 
         
     }
